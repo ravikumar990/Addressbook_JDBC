2849cd22269b2229abde4ea4af5ced0cba12b295
ravi <ravi@gmail.com>
2021-11-21 06:35:57 +0530

    retrive all data from database

---
+++ src/main/java/com/addressbook/AddressBook.java
@@ -0,0 +1,24 @@
+package com.addressbook;
+
+import java.util.List;
+
+public class AddressBook {
+	private AddressBookDB addressBookDB;
+	private List<PersonInformation> contacts;
+
+	public enum IOService {
+		DB_IO
+	}
+
+	public AddressBook() {
+		addressBookDB = AddressBookDB.getInstance();
+	}
+
+	public List<PersonInformation> readContactData(IOService ioService) {
+		if (ioService.equals(IOService.DB_IO))
+			this.contacts = addressBookDB.readData();
+		System.out.println(this.contacts);
+		return this.contacts;
+	}
+
+}
